de883f6842570fe475155a51fce8615dd8ef1c07
petrazhitskiy <148198616+petrazhitskiy@users.noreply.github.com>
2024-10-04 16:42:03 +0300

    Изменил замер времени

--- 1_laba_device.cu
+++ 1_laba_device.cu
@@ -51,29 +51,34 @@ int main(int argc, char** argv) {
     CHECK_CUDA_ERROR(cudaEventCreate(&t1));
     CHECK_CUDA_ERROR(cudaEventCreate(&t2));
 
-    cudaEvent_t copy_start, copy_stop;
-    CHECK_CUDA_ERROR(cudaEventCreate(&copy_start));
-    CHECK_CUDA_ERROR(cudaEventCreate(&copy_stop));
+    cudaEvent_t copy_start_1, copy_stop_1;
+    cudaEvent_t copy_start_2, copy_stop_2;
+    CHECK_CUDA_ERROR(cudaEventCreate(&copy_start_1));
+    CHECK_CUDA_ERROR(cudaEventCreate(&copy_stop_1));
+    CHECK_CUDA_ERROR(cudaEventCreate(&copy_start_2));
+    CHECK_CUDA_ERROR(cudaEventCreate(&copy_stop_2));
 
-    CHECK_CUDA_ERROR(cudaEventRecord(copy_start));
+    CHECK_CUDA_ERROR(cudaEventRecord(copy_start_1));
     CHECK_CUDA_ERROR(cudaMemcpy(d_a, h_a, N * sizeof(double), cudaMemcpyHostToDevice));
     CHECK_CUDA_ERROR(cudaMemcpy(d_b, h_b, N * sizeof(double), cudaMemcpyHostToDevice));
-    CHECK_CUDA_ERROR(cudaEventRecord(copy_stop));
+    CHECK_CUDA_ERROR(cudaEventRecord(copy_stop_1));
 
     int threads = 128;
     int blocks = N / threads + 1;
     CHECK_CUDA_ERROR(cudaEventRecord(t1));
     sumvec_d<<<blocks, threads>>>(d_a, d_b, d_c, N);
-    CHECK_CUDA_ERROR(cudaMemcpy(h_c, d_c, N * sizeof(double), cudaMemcpyDeviceToHost));
     CHECK_CUDA_ERROR(cudaEventRecord(t2));
+    CHECK_CUDA_ERROR(cudaEventRecord(copy_start_1));
+    CHECK_CUDA_ERROR(cudaMemcpy(h_c, d_c, N * sizeof(double), cudaMemcpyDeviceToHost));
+    CHECK_CUDA_ERROR(cudaEventRecord(copy_stop_2));
     CHECK_CUDA_ERROR(cudaDeviceSynchronize());
 
-    float t_kernel = 0, t_copy = 0;
+    float t_kernel = 0, t_copy_1 = 0, t_copy_2 = 0;
     CHECK_CUDA_ERROR(cudaEventElapsedTime(&t_kernel, t1, t2));  
-    CHECK_CUDA_ERROR(cudaEventElapsedTime(&t_copy, copy_start, copy_stop)); 
-    std::cout << "t_kernel: " << t_kernel << " ms" << std::endl;
-    std::cout << "t_copy: " << t_copy << " ms" << std::endl;
-
+    CHECK_CUDA_ERROR(cudaEventElapsedTime(&t_copy1, copy_start_1, copy_stop_1)); 
+    CHECK_CUDA_ERROR(cudaEventElapsedTime(&t_copy2, copy_start_2, copy_stop_1)); 
+    std::cout << "t_kernel: " << t_kernel  << " ms" << std::endl;
+    std::cout << "t_copy: " << t_copy_1 + t_copy_2 << " ms" << std::endl;
 
     free(h_a);
     free(h_b);
